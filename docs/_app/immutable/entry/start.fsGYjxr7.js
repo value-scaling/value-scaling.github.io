da84c3dc192c7cf4ca2e886a9b098e5650718220
prestonfu <prestonfu@berkeley.edu>
2025-07-05 19:25:13 -0700

    gh pages build

---
+++ docs/_app/immutable/entry/start.fsGYjxr7.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bw37SNoi.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from "@sveltejs/adapter-vercel";
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [preprocess({ postcss: true })],
   kit: {
-    adapter: adapter(),
-  },
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null
+    }),
+    paths: {
+      base: ''
+    }
+  }
 };
+
 export default config;
